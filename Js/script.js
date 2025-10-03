c5c17825ea8769c33d81c074b4dfaabd7342a132
EnzoIOliveira <ifanger08@gmail.com>
2025-10-02 22:32:09 -0300

    site quase terminado, só falta imagens

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,5 @@
 const accordionHeaders = document.querySelectorAll(".accordion-header");
 const accordionContents = document.querySelectorAll(".accordion-content");
-const imagemLateral = document.getElementById("imagem-lateral");
 
 accordionHeaders.forEach((header) => {
     header.addEventListener("click", () => {
@@ -8,9 +7,22 @@ accordionHeaders.forEach((header) => {
         const accordionContent = accordionItem.querySelector(".accordion-content");
         const isActive = accordionContent.classList.contains("active");
 
-        const novaImagem = header.getAttribute("dat-img");
-        imagemLateral.setAttribute("src", novaImagem);
+        const headers = document.querySelectorAll(".accordion-header");
+        const images = document.querySelectorAll(".image-area img");
 
+        headers.forEach(header => {
+            header.addEventListener("click", () => {
+                const item = header.parentElement;
+                const imgId = item.getAttribute("data-img"); // pega o número da imagem
+
+                // remove active de todas
+                images.forEach(img => img.style.display = "none");
+
+                // mostra só a certa
+                const target = document.getElementById("img" + imgId);
+                if (target) target.style.display = "block";
+            });
+        });
         // Fecha todos os outros
         accordionContents.forEach((content) => {
             content.classList.remove("active");
@@ -24,5 +36,23 @@ accordionHeaders.forEach((header) => {
             accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
             accordionContent.style.padding = "10px";
         }
+    });
+});
+
+
+const headers = document.querySelectorAll(".accordion-header");
+const images = document.querySelectorAll(".image-area img");
+
+headers.forEach(header => {
+    header.addEventListener("click", () => {
+        const item = header.parentElement;
+        const imgId = item.getAttribute("data-img");
+
+        // esconde todas
+        images.forEach(img => img.style.display = "none");
+
+        // mostra só a certa
+        const target = document.getElementById("img" + imgId);
+        if (target) target.style.display = "block";
     });
 });
